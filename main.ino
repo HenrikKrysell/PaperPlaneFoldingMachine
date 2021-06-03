b53f7c22dba470a6b13d99f2f0f2f265bf2747dc
Henrik Krysell <henrikkrysell@gmail.com>
2021-06-03 16:35:40 +0200

    Haxande

--- main.ino
+++ main.ino
@@ -1,7 +1,16 @@
 #include <Servo.h>
-#define B_PIN 1
+#define B_PIN 3
 
 bool isRun = false;
+
+// int SERVO_MIN = 544;
+// int SERVO_MAX = 2400;
+int SERVO_MIN = 0;
+int SERVO_MAX = 180;
+int servo1_left_up_startPos = SERVO_MIN;
+int servo1_left_up_endPos = SERVO_MAX;
+int servo2_right_up_startPos = SERVO_MAX;
+int servo2_right_up_endPos = SERVO_MIN;
 
 Servo servo1_left_up;
 Servo servo2_right_up;
@@ -13,15 +22,15 @@ Servo servo6_ext_left;
 void setup() {
   Serial.begin(115200);
 
-  pinMode(B_PIN, INPUT_PULLUP);
+  //pinMode(B_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(B_PIN), buttonChanged, RISING);
 
-  servo1_left_up.attach(2); // attaches the servo on pin 1 to the servo object
-  servo2_right_up.attach(3);  
-  servo3_left_down.attach(4);
-  servo4_right_down.attach(5);
-  servo5_ext_right.attach(6);
-  servo6_ext_left.attach(7);
+  servo1_left_up.attach(7); // attaches the servo on pin 1 to the servo object
+  servo2_right_up.attach(6);  
+  // servo3_left_down.attach(4);
+  // servo4_right_down.attach(5);
+  // servo5_ext_right.attach(6);
+  // servo6_ext_left.attach(7);
 }
 
 void loop() {
@@ -31,30 +40,36 @@ void loop() {
 
   reset();
 
+  Serial.println("Folding BEGIN");
+
   // Fold top left side
-  opServo(servo1_left_up, 180);
-  opServo(servo1_left_up, 0);
+  Serial.println("Folding Top Left");
+  opServo(servo1_left_up, servo1_left_up_endPos);
+  opServo(servo1_left_up, servo1_left_up_startPos);
 
   // Fold top right side
-  opServo(servo2_right_up, 180);
-  opServo(servo2_right_up, 0);
+  Serial.println("Folding Top Right");
+  opServo(servo2_right_up, servo2_right_up_endPos);
+  opServo(servo2_right_up, servo2_right_up_startPos);
   
-  // Fold bottom left side
-  opServo(servo3_left_down, 180);
-  opServo(servo3_left_down, 0);
+  // // Fold bottom left side
+  // opServo(servo3_left_down, 180);
+  // opServo(servo3_left_down, 0);
 
-  // Fold bottom right side
-  opServo(servo4_right_down, 180);
-  opServo(servo4_right_down, 0);
+  // // Fold bottom right side
+  // opServo(servo4_right_down, 180);
+  // opServo(servo4_right_down, 0);
+
+  isRun = false;
 }
 
 void reset() {
-  opServo(servo1_left_up, 0);
-  opServo(servo2_right_up, 0);
-  opServo(servo3_left_down, 0);
-  opServo(servo4_right_down, 0);
-  opServo(servo5_ext_right, 0);
-  opServo(servo6_ext_left, 0);
+  opServo(servo1_left_up, servo1_left_up_startPos);
+  opServo(servo2_right_up, servo2_right_up_startPos);
+  // opServo(servo3_left_down, 0);
+  // opServo(servo4_right_down, 0);
+  // opServo(servo5_ext_right, 0);
+  // opServo(servo6_ext_left, 0);
 }
 
 void opServo(Servo servo, int targetPos) {
@@ -63,16 +78,16 @@ void opServo(Servo servo, int targetPos) {
   }
 
   int currPos = servo.read();
+  Serial.println(currPos);
   while (currPos != targetPos) {
     if (isRun) {
       currPos += currPos < targetPos ? 1 : -1;
       servo.write(currPos);
     }
-    delay(50);
+    delay(15);
   }
 }
 
 void buttonChanged() {
-  isRun != isRun;
-  delay(15);
+  isRun = true;
 }
